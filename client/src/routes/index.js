36a56aa8806c3f0ff2931db1a2bc05bda469b443
Bilal E <bilal.hyni@gmail.com>
2026-01-21 23:49:43 +0000

    feat: unify login redirect to /home for all users
    - All users now redirect to /home after successful login
    - Add PrivateRouteAuth component for general authentication
    - Add unified /home route accessible by all authenticated users
    - Role-based filtering handled by Dashboard component

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -2,3 +2,4 @@ export { default as PrivateRoute } from './PrivateRoute';
 export { default as PrivateRouteProf } from './PrivateRouteProf';
 export { default as PrivateRouteAdmin } from './PrivateRouteAdmin';
 export { default as PrivateRouteStudent } from './PrivateRouteStudent';
+export { default as PrivateRouteAuth } from './PrivateRouteAuth';
